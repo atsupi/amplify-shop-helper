620537d77150919674388c0a6dda27340bee24f5
atsupi <atsupi@gmail.com>
2023-04-05 00:05:29 +0900

    Added Back button on Detail page.

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@aws-amplify/ui-react";
+import { Link } from "react-router-dom";
 import "./Detail.css";
 
 function Detail() {
   const OnBack = () => {
     console.log("Back pressed");
+    this.props.history.push
   };
   return (
     <>
       <p>Detail</p>
-      <Button onClick={OnBack}>Back</Button>
+      <Link to="/purchase">Back</Link>
     </>
   );
 }
